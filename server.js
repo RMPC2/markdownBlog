ad0a4755bae9d4a91d3a6b2a90c01e3dce746f94
Thomas Campbell <thomas.campbell@ecs.co.uk>
2022-10-12 18:32:49 +0100

    changes up to 19:10

--- server.js
+++ server.js
@@ -7,7 +7,18 @@ app.set('view engine', 'ejs')
 app.use('/articles', articleRouter)
 
 app.get('/',(req, res) => {
-    res.render('index')
+    const articles = [{
+        title: 'Test Article',
+        createdAt: new Date(),
+        description: 'Test Description' 
+    },
+    {
+        title: 'Test Article 2',
+        createdAt: new Date(),
+        description: 'Test Description 2'
+    }]
+
+    res.render('articles/index', { articles: articles })
 })
 
 app.listen(3000)
